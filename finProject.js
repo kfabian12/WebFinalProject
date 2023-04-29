33945c8fb2e2a1a097bbf49748aa911c1ce09964
kfabian12 <120209973+kfabian12@users.noreply.github.com>
2023-04-28 23:12:40 -0400

    javascript

---
+++ finProject.js
@@ -0,0 +1,7 @@
+var btn = document.getElementById("submitButton");
+
+btn.addEventListener("click", submit());
+
+function submit() {
+    alert("You succesfully submitted the form! I will email you soon.");
+}
