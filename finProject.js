8fbe5f732fad9675c866912b652c514809199601
kfabian12 <120209973+kfabian12@users.noreply.github.com>
2023-05-02 19:57:59 -0400

    Update finProject.js

--- finProject.js
+++ finProject.js
@@ -1,7 +1,19 @@
-var btn = document.getElementById("submitButton");
+var body = document.getElementsByTagName('body')[0];
+body.style.backgroundColor = 'rgb(112, 192, 230)';
 
-btn.addEventListener("click", submit());
-
-function submit() {
-    alert("You succesfully submitted the form! I will email you soon.");
+window.onscroll = function (event) {
+    var scroll = window.pageYOffset;
+    if (scroll < 700) {
+        // green
+        body.style.backgroundColor = 'rgb(112, 192, 230)';
+    } else if (scroll >= 700 && scroll < 1400) {
+        // yellow
+        body.style.backgroundColor = 'rgb(80, 107, 199)';
+    } else if (scroll >= 1400 && scroll < 2100) {
+        // yellow
+        body.style.backgroundColor = 'rgb(120, 56, 199)';
+    } else {
+        // purple
+        body.style.backgroundColor = 'rgb(176, 134, 227)';
+    }
 }
